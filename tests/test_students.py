65550332134d8c336624fa5a31974757d6b00aaa
Maciej <maciej.tokarz1@wp.pl>
2020-11-29 20:20:14 +0100

    test_add_student_bad_option RED

--- tests/test_students.py
+++ tests/test_students.py
@@ -7,6 +7,10 @@ class StudentsAssertPyTest(unittest.TestCase):
     def setUp(self):
         self.temp = Students()
 
+    def test_add_student_bad_option(self):
+        assert_that(self.temp.addStudent) \
+            .raises(Exception) \
+            .when_called_with(2, "Jan", "Kowalski", "g")
     def test_add_student_surname_bad_type(self):
         assert_that(self.temp.addStudent) \
             .raises(TypeError) \
