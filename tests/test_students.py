915b19349da549c49132236ec875ea0e1f300fae
Maciej <maciej.tokarz1@wp.pl>
2020-11-30 00:01:50 +0100

    test_edit_type_error RED

--- tests/test_students.py
+++ tests/test_students.py
@@ -8,6 +8,13 @@ class StudentsAssertPyTest(unittest.TestCase):
     def setUp(self):
         self.temp = Students()
 
+    def test_edit_type_error(self):
+        self.temp.addStudent(1, "Ala", "Poziomka", "w")
+        self.temp.addStudent(2, "Marcel", "Bialy", "a")
+        assert_that(self.temp.editStudent) \
+            .raises(TypeError) \
+            .when_called_with(2, "Marcel", "Bialy", None, "Pola")
+
     def test_delete_lack_student(self):
         self.temp.addStudent(1, "Kasia", "Polak", "w")
         self.temp.addStudent(2, "Kacper", "Stoch", "a")
