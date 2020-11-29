ffa1557e38b68ba22717c104096cd723bab2a9fa
Maciej <maciej.tokarz1@wp.pl>
2020-11-29 20:11:07 +0100

    test_add_student_name_bad_type REFACTOR

--- src/students.py
+++ src/students.py
@@ -3,6 +3,5 @@ class Students:
         self.students = {}
 
     def addStudent(self, id, name, surname, option):
-        raise TypeError("Bad_type_name_or_surname")
-
-
+        if type(name) != str:
+            raise TypeError("Bad_type_name_or_surname")
--- tests/test_students.py
+++ tests/test_students.py
@@ -3,10 +3,10 @@ from assertpy import assert_that
 from src.students import Students
 
 
-
 class StudentsAssertPyTest(unittest.TestCase):
     def setUp(self):
         self.temp = Students()
+
     def test_add_student_name_bad_type(self):
         assert_that(self.temp.addStudent) \
             .raises(TypeError).when_called_with(2, True, "Kowalski", "w")
