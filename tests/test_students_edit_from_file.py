c1ba4d1ecb9c08ca352dbb1ffe6120da7f6d09a2
Maciej <maciej.tokarz1@wp.pl>
2020-12-03 22:21:40 +0100

    test_student_edit_from_file_exceptions

--- tests/test_students_edit_from_file.py
+++ tests/test_students_edit_from_file.py
@@ -17,6 +17,18 @@ class StudentsParameterizedFile(unittest.TestCase):
                 self.assertIn(tmpStudents.editStudent(inp1, inp2, inp3, inp4, inp5)[0][1], inp6)
         fileTest.close()
 
+    def test_student_edit_from_file_exceptions(self):
+        fileTest = open("data/test_students_edit_exception.txt")
+        tmpStudents = Students()
+        for line in fileTest:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                data = line.split(" ")
+                inp1, inp2, inp3, inp4, inp5, inp6 = (data[0], data[1], data[2], data[3], data[4], data[5].strip("\n"))
+                self.assertRaisesRegex(Exception, inp6, tmpStudents.editStudent, inp1, inp2, inp3, inp4, inp5)
+        fileTest.close()
+
 
 if __name__ == '__main__':
     unittest.main()
