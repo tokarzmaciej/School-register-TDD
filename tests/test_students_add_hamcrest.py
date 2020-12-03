42405e1e827c22f4d78b9732fe3f3756b7d7932e
Maciej <maciej.tokarz1@wp.pl>
2020-12-03 23:19:03 +0100

    test_add_student_name_bad_type hamcrest

--- tests/test_students_add_hamcrest.py
+++ tests/test_students_add_hamcrest.py
@@ -22,5 +22,10 @@ class StudentsHamcrestTest(unittest.TestCase):
                     .with_args(generate().get_key(), "Jan", 2),
                     raises(TypeError))
 
+    def test_add_student_name_bad_type(self):
+        assert_that(calling(self.temp.addStudent)
+                    .with_args(generate().get_key(), True, "Kowalski"),
+                    raises(TypeError))
+
     def tearDown(self):
         self.temp = None
