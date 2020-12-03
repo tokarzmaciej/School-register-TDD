f0500c7bc3dfb48d8bb02a8653d86cd1d3ca9f71
Maciej <maciej.tokarz1@wp.pl>
2020-12-03 23:34:05 +0100

    test_edit_lack_student hamcrest

--- tests/test_students_edit_hamcrest.py
+++ tests/test_students_edit_hamcrest.py
@@ -11,6 +11,10 @@ class StudentsAssertPyTest(unittest.TestCase):
         assert_that(self.temp.editStudent(1, "Kasia", "Polak", "Asia", "Pola")[0][1],
                     contains_string("Asia"))
 
+    def test_edit_lack_student(self):
+        assert_that(calling(self.temp.editStudent)
+                    .with_args(2, "Aneta", "Czarna", "Asia", "Pola"),
+                    raises(Exception))
 
     def tearDown(self):
         self.temp = None
