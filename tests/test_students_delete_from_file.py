8977d08d5886057c361cc5ffd14da4197dbaf59f
Maciej <maciej.tokarz1@wp.pl>
2020-12-03 22:07:02 +0100

    test_student_delete_from_file

---
+++ tests/test_students_delete_from_file.py
@@ -0,0 +1,22 @@
+import unittest
+from src.students import *
+
+
+class StudentsParameterizedFile(unittest.TestCase):
+
+    def test_student_delete_from_file(self):
+        fileTest = open("data/test_students_delete.txt")
+        tmpStudents = Students()
+        for line in fileTest:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                data = line.split(" ")
+                inp1, inp2, inp3, inp4, inp5, inp6 = (
+                    data[0], data[1], data[2], data[3], data[4], data[5].strip("\n"))
+                self.assertNotIn(tmpStudents.deleteStudent(inp1, inp2, inp3), (inp4, inp5, inp6))
+        fileTest.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
