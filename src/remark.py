fb754ff97b1b157039a30a091d35b2282346907a
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 21:47:40 +0100

    test_add_remark_already_exists GREEN

--- src/remark.py
+++ src/remark.py
@@ -4,5 +4,8 @@ from src.students import *
 class Remarks(Students):
 
     def addRemark(self, id, name_student, surname_student, name_remark, description_remark):
-        if not self.keys.__contains__((str(id), name_student, surname_student)):
+        if self.keys.__contains__((str(id), name_student, surname_student)):
+            if [*self.students[(str(id), name_student, surname_student)]["remarks"]].__contains__(name_remark):
+                raise Exception("This_remark_already_exists")
+        else:
             raise Exception("There_is_not_such_student")
