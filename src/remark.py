90a7f782776bdbcff9ca391e0ab4fa5d7ecfb564
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 21:49:09 +0100

    test_add_remark_already_exists REFACTOR

--- src/remark.py
+++ src/remark.py
@@ -5,7 +5,8 @@ class Remarks(Students):
 
     def addRemark(self, id, name_student, surname_student, name_remark, description_remark):
         if self.keys.__contains__((str(id), name_student, surname_student)):
-            if [*self.students[(str(id), name_student, surname_student)]["remarks"]].__contains__(name_remark):
+            valueRemarks = self.students[(str(id), name_student, surname_student)]["remarks"]
+            if [*valueRemarks].__contains__(name_remark):
                 raise Exception("This_remark_already_exists")
         else:
             raise Exception("There_is_not_such_student")
