0f093b34823dac75917ef567bf42df1b8cd99980
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 10:08:29 +0100

    test_delete_subject_name_bad_type RED

--- src/subjects.py
+++ src/subjects.py
@@ -17,3 +17,4 @@ class Subjects(Students):
     def deleteSubject(self, id, name_student, surname_student, name_subject):
         if not self.keys.__contains__((str(id), name_student, surname_student)):
             raise Exception("There_is_not_such_student")
+
--- tests/test_subjects_delete.py
+++ tests/test_subjects_delete.py
@@ -8,6 +8,10 @@ class SubjectsAssertPyTest(unittest.TestCase):
     def setUp(self):
         self.temp = Subjects()
 
+    def test_delete_subject_name_bad_type(self):
+        assert_that(self.temp.deleteSubject) \
+            .raises(TypeError).when_called_with(5, "Piotr", "Fantazja", 1234)
+
     def test_delete_subject_to_lack_student(self):
         assert_that(self.temp.deleteSubject) \
             .raises(Exception).when_called_with(10, "Ania", "Zegan", "english")
