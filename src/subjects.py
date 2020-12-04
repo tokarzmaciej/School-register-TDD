539e898ed9d1f8edde8ba192a4765d75394052fb
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 11:32:01 +0100

    test_edit_subject_positive REFACTOR

--- src/subjects.py
+++ src/subjects.py
@@ -31,14 +31,13 @@ class Subjects(Students):
         if type(new_name_subject) != str:
             raise TypeError("Bad_type_new_subject_name")
         else:
-            if not self.keys.__contains__((str(id), name_student, surname_student)):
-                raise Exception("There_is_not_such_student")
-            else:
-                if not [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(
-                        name_subject):
-                    raise Exception("Student_not_have_this_subject")
-                else:
+            if self.keys.__contains__((str(id), name_student, surname_student)):
+                if [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(name_subject):
                     self.students[(str(id), name_student, surname_student)]["subjects"][new_name_subject] = \
                         self.students[(str(id), name_student, surname_student)]["subjects"].pop(name_subject)
                     return [*self.students[(str(id), name_student, surname_student)]["subjects"]]
+                else:
+                    raise Exception("Student_not_have_this_subject")
 
+            else:
+                raise Exception("There_is_not_such_student")
