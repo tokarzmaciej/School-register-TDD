26a5a29f4840adc2d993a71757ad1ad00e340bae
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 22:21:35 +0100

    test_edit_remark_description_lack_remark GREEN

--- src/remark.py
+++ src/remark.py
@@ -21,5 +21,7 @@ class Remarks(Students):
                     name_remark):
                 self.students[(str(id), name_student, surname_student)]["remarks"][name_remark] = new_description_remark
                 return self.students[(str(id), name_student, surname_student)]["remarks"][name_remark]
+            else:
+                raise Exception("Student_not_have_this_remark")
         else:
             raise Exception("There_is_not_such_student")
