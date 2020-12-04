07f25c5ae9d9695c342d76cff1f58ca64fd34e71
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 19:31:35 +0100

    test_add_mark_name_bad_type RED

---
+++ src/marks.py
@@ -0,0 +1,7 @@
+from src.students import *
+
+
+class Marks(Students):
+
+    def addMark(self, id, name_student, surname_student, name_subject, name_mark, grade):
+      return ""
---
+++ tests/test_marks.py
@@ -0,0 +1,17 @@
+import unittest
+from assertpy import assert_that
+from src.marks import Marks
+
+
+class SubjectsAssertPyTest(unittest.TestCase):
+
+    def setUp(self):
+        self.temp = Marks()
+
+    def test_add_mark_name_bad_type(self):
+        assert_that(self.temp.addMark) \
+            .raises(TypeError).when_called_with(5, "Piotr", "Fantazja", "math", "test", "1")
+
+
+    def tearDown(self):
+        self.temp = None
