c2955d5fbf7e7572f56c26804f40f3ba1b73e8b6
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 21:53:29 +0100

    test_add_remark_positive RED

--- tests/test_add_remark.py
+++ tests/test_add_remark.py
@@ -8,6 +8,11 @@ class RemarksAddAssertPyTest(unittest.TestCase):
     def setUp(self):
         self.temp = Remarks()
 
+    def test_add_remark_positive(self):
+        result = {'volunteering': 'cleaning_the_environment'}
+        assert_that(self.temp.addRemark(2, "Beata", "Jankowska", "volunteering", "cleaning_the_environment")) \
+            .is_equal_to(result)
+
     def test_add_remark_already_exists(self):
         assert_that(self.temp.addRemark) \
             .raises(Exception).when_called_with(8, "Konrad", "Piasek", "competition", "first place")
