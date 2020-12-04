fb3aa12482351c4df44940ed8e5d2e72c0f13efe
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 11:17:50 +0100

    test_edit_subject_to_lack_subjects REFACTOR

--- src/subjects.py
+++ src/subjects.py
@@ -34,4 +34,6 @@ class Subjects(Students):
             if not self.keys.__contains__((str(id), name_student, surname_student)):
                 raise Exception("There_is_not_such_student")
             else:
-                raise Exception("Student_not_have_this_subject")
+                if not [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(
+                        name_subject):
+                    raise Exception("Student_not_have_this_subject")
