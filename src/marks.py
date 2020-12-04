e6a50815eae3e943453d79d318c963f3508ade1b
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 20:02:12 +0100

    test_add_mark_range_mark RED

--- src/marks.py
+++ src/marks.py
@@ -6,3 +6,4 @@ class Marks(Students):
     def addMark(self, id, name_student, surname_student, name_subject, name_mark, grade):
         if type(grade) != int:
             raise TypeError("Bad_type_grade")
+
--- tests/test_marks.py
+++ tests/test_marks.py
@@ -8,6 +8,10 @@ class SubjectsAssertPyTest(unittest.TestCase):
     def setUp(self):
         self.temp = Marks()
 
+    def test_add_mark_range_mark(self):
+        assert_that(self.temp.addMark) \
+            .raises(Exception).when_called_with(4, "Alicja", "Zielonka", "math", "test", 7)
+
     def test_add_mark_name_bad_type(self):
         assert_that(self.temp.addMark) \
             .raises(TypeError).when_called_with(5, "Piotr", "Fantazja", "math", "test", "1")
