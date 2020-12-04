f902e4e82bc540dcabf9ba1c5273d1915f4d1880
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 11:07:13 +0100

    test_edit_subject_to_lack_student RED

--- tests/test_subjects_edit.py
+++ tests/test_subjects_edit.py
@@ -8,6 +8,10 @@ class SubjectsAssertPyTest(unittest.TestCase):
     def setUp(self):
         self.temp = Subjects()
 
+    def test_edit_subject_to_lack_student(self):
+        assert_that(self.temp.editSubject) \
+            .raises(Exception).when_called_with(2, "Grzegorz", "Kowalczyk", "english", "primary_english")
+
     def test_edit_subject_name_bad_type(self):
         assert_that(self.temp.editSubject) \
             .raises(TypeError).when_called_with(5, "Piotr", "Fantazja", "history", False)
