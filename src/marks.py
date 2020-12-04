53e89d9c30b77a0ec21114ef01c95d20a522d944
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 20:00:08 +0100

    test_add_mark_name_bad_type REFACTOR

--- src/marks.py
+++ src/marks.py
@@ -4,4 +4,5 @@ from src.students import *
 class Marks(Students):
 
     def addMark(self, id, name_student, surname_student, name_subject, name_mark, grade):
-      raise TypeError("Bad_type_grade")
+        if type(grade) != int:
+            raise TypeError("Bad_type_grade")
