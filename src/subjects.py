b052450a93a5f4211b2ea8196852f413a5e411e4
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 09:29:50 +0100

    test_add_subject_to_lack_student GREEN

--- src/subjects.py
+++ src/subjects.py
@@ -4,5 +4,9 @@ from src.students import *
 class Subjects(Students):
 
     def addSubject(self, id, name_student, surname_student, name_subject):
+        keys = [*self.students]
         if type(name_subject) != str:
             raise TypeError("Bad_type_subject_name")
+        else:
+            raise Exception("There_is_not_such_student")
+
