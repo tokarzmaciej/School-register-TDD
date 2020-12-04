1f52f7cac33d58a2c9ae8284d0450aa1a4ce97fd
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 23:36:54 +0100

    test_average_subject_positive GREEN

--- src/statistics.py
+++ src/statistics.py
@@ -6,8 +6,13 @@ class Statistics(Students):
     def averageSubject(self, id, name_student, surname_student, name_subject):
         keys = [*self.students]
         if keys.__contains__((str(id), name_student, surname_student)):
-            if not [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(
+            if [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(
                     name_subject):
+
+                values = self.students[(str(id), name_student, surname_student)]["subjects"][name_subject].values()
+                if values:
+                    return "average" + " " + name_subject, sum(values) / len(values)
+            else:
                 raise Exception("Student_not_have_this_subject")
         else:
             raise Exception("There_is_not_such_student")
