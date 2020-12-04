1df94863d8c55bf72d55f3df051cb5e99584ba92
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 21:39:06 +0100

    test_add_remark_lack_student REFACTOR

--- src/remark.py
+++ src/remark.py
@@ -4,5 +4,5 @@ from src.students import *
 class Remarks(Students):
 
     def addRemark(self, id, name_student, surname_student, name_remark, description_remark):
-        raise Exception("There_is_not_such_student")
-
+        if not self.keys.__contains__((str(id), name_student, surname_student)):
+            raise Exception("There_is_not_such_student")
