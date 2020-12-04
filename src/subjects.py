87ff2d17f9f659d443e995a86ecb27d2cbbbc3f3
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 10:38:50 +0100

    test_delete_subject_positive REFACTOR

--- src/subjects.py
+++ src/subjects.py
@@ -18,12 +18,11 @@ class Subjects(Students):
         if type(name_subject) != str:
             raise TypeError("Bad_type_subject_name")
         else:
-            if not self.keys.__contains__((str(id), name_student, surname_student)):
-                raise Exception("There_is_not_such_student")
-            else:
-                if not [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(
-                        name_subject):
-                    raise Exception("Student_not_have_this_subject")
-                else:
+            if self.keys.__contains__((str(id), name_student, surname_student)):
+                if [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(name_subject):
                     self.students[(str(id), name_student, surname_student)]["subjects"].pop(name_subject)
                     return self.students[(str(id), name_student, surname_student)]
+                else:
+                    raise Exception("Student_not_have_this_subject")
+            else:
+                raise Exception("There_is_not_such_student")
