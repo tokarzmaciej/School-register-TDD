16808392bb853e13198fc14ca7380a0bc2762fd7
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 09:32:31 +0100

    test_add_subject_to_lack_student REFACTOR

--- src/subjects.py
+++ src/subjects.py
@@ -7,6 +7,5 @@ class Subjects(Students):
         keys = [*self.students]
         if type(name_subject) != str:
             raise TypeError("Bad_type_subject_name")
-        else:
+        elif not keys.__contains__((str(id), name_student, surname_student)):
             raise Exception("There_is_not_such_student")
-
