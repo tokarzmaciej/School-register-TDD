25c706d344cb9594c00c01c22d0f0c5806f5a7bd
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 22:12:21 +0100

    test_edit_mark_description_positive RED

--- tests/test_edit_remark.py
+++ tests/test_edit_remark.py
@@ -8,6 +8,10 @@ class RemarksAddAssertPyTest(unittest.TestCase):
     def setUp(self):
         self.temp = Remarks()
 
+    def test_edit_mark_description_positive(self):
+        assert_that(self.temp.editRemarkDescription(7, "Ewelina", "Swoboda", "competition", "second_place")) \
+            .starts_with("second")
+
     def test_edit_remark_description_lack_students(self):
         assert_that(self.temp.editRemarkDescription) \
             .raises(Exception).when_called_with(14, "Julka", "Ostrowska", "competition", "first_place")
