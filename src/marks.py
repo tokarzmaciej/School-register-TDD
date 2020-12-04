9b0d60bc209294a6d62de52e202978e81f4474be
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 20:04:59 +0100

    test_add_mark_range_mark REFACTOR

--- src/marks.py
+++ src/marks.py
@@ -6,5 +6,5 @@ class Marks(Students):
     def addMark(self, id, name_student, surname_student, name_subject, name_mark, grade):
         if type(grade) != int:
             raise TypeError("Bad_type_grade")
-        else:
+        elif grade > 6 or grade < 1:
             raise Exception("Bad_range_marks")
