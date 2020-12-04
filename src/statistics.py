b94355a447d504ba81c6ec01808ec37786177f20
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 23:21:40 +0100

    test_average_subject_lack_student GREEN

--- src/statistics.py
+++ src/statistics.py
@@ -4,4 +4,4 @@ from src.students import *
 class Statistics(Students):
 
     def averageSubject(self, id, name_student, surname_student, name_subject):
-        return ""
+        raise Exception("There_is_not_such_student")
