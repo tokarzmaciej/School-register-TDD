701bf7b9e9c145bd5d77ecc0e316464e21a66bd1
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 23:29:35 +0100

    test_average_subject_lack_subject GREEN

--- src/statistics.py
+++ src/statistics.py
@@ -6,3 +6,5 @@ class Statistics(Students):
     def averageSubject(self, id, name_student, surname_student, name_subject):
         if not self.keys.__contains__((str(id), name_student, surname_student)):
             raise Exception("There_is_not_such_student")
+        elif not [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(name_subject):
+            raise Exception("Student_not_have_this_subject")
