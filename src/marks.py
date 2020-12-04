6f107a50686c212868fcf43be27b632ec834f2a9
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 20:18:19 +0100

    test_add_mark_lack_subjects REFACTOR

--- src/marks.py
+++ src/marks.py
@@ -9,8 +9,13 @@ class Marks(Students):
         elif grade > 6 or grade < 1:
             raise Exception("Bad_range_marks")
         else:
-            if not self.keys.__contains__((str(id), name_student, surname_student)):
-                raise Exception("There_is_not_such_student")
+            if self.keys.__contains__((str(id), name_student, surname_student)):
+                if [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(
+                        name_subject):
+                    raise Exception("This_mark_already_exists")
+                else:
+                    raise Exception("Student_not_have_this_subject")
             else:
-                raise Exception("Student_not_have_this_subject")
+                raise Exception("There_is_not_such_student")
+
 
