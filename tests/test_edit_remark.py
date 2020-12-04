a0370bed3f7d24030eb8def1ccfa936f4db0058a
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 22:20:02 +0100

    test_edit_remark_description_lack_remark RED

--- tests/test_edit_remark.py
+++ tests/test_edit_remark.py
@@ -12,6 +12,10 @@ class RemarksAddAssertPyTest(unittest.TestCase):
         assert_that(self.temp.editRemarkDescription(7, "Ewelina", "Swoboda", "competition", "second_place")) \
             .starts_with("second")
 
+    def test_edit_remark_description_lack_remark(self):
+        assert_that(self.temp.editRemarkDescription) \
+            .raises(Exception).when_called_with(3, "Kacper", "Stoch", "volunteering", "helping_an_elderly_person")
+
     def test_edit_remark_description_lack_students(self):
         assert_that(self.temp.editRemarkDescription) \
             .raises(Exception).when_called_with(14, "Julka", "Ostrowska", "competition", "first_place")
