04a510981b2ba4f62001325add057304367e17ca
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 22:59:45 +0100

    test_edit_remark_name_lack_remark REFACTOR

--- src/remark.py
+++ src/remark.py
@@ -3,20 +3,20 @@ from src.students import *
 
 class Remarks(Students):
 
-    def addRemark(self, id, name_student, surname_student, name_remark, description_remark):
-        if self.keys.__contains__((str(id), name_student, surname_student)):
-            valueRemarks = self.students[(str(id), name_student, surname_student)]["remarks"]
+    def addRemark(self, id_student, name_student, surname_student, name_remark, description_remark):
+        if self.keys.__contains__((str(id_student), name_student, surname_student)):
+            valueRemarks = self.students[(str(id_student), name_student, surname_student)]["remarks"]
             if [*valueRemarks].__contains__(name_remark):
                 raise Exception("This_remark_already_exists")
             else:
                 valueRemarks[name_remark] = description_remark
-                return self.students[(str(id), name_student, surname_student)]["remarks"]
+                return self.students[(str(id_student), name_student, surname_student)]["remarks"]
         else:
             raise Exception("There_is_not_such_student")
 
-    def editRemarkDescription(self, id, name_student, surname_student, name_remark, new_description_remark):
-        if self.keys.__contains__((str(id), name_student, surname_student)):
-            valueRemarks = self.students[(str(id), name_student, surname_student)]["remarks"]
+    def editRemarkDescription(self, id_student, name_student, surname_student, name_remark, new_description_remark):
+        if self.keys.__contains__((str(id_student), name_student, surname_student)):
+            valueRemarks = self.students[(str(id_student), name_student, surname_student)]["remarks"]
             if [*valueRemarks].__contains__(
                     name_remark):
                 valueRemarks[name_remark] = new_description_remark
@@ -25,9 +25,10 @@ class Remarks(Students):
                 raise Exception("Student_not_have_this_remark")
         else:
             raise Exception("There_is_not_such_student")
-    def editRemarkName(self, id, name_student, surname_student, name_remark, new_name_remark):
-        if self.keys.__contains__((str(id), name_student, surname_student)):
-            valueRemarks = self.students[(str(id), name_student, surname_student)]["remarks"]
+
+    def editRemarkName(self, id_student, name_student, surname_student, name_remark, new_name_remark):
+        if self.keys.__contains__((str(id_student), name_student, surname_student)):
+            valueRemarks = self.students[(str(id_student), name_student, surname_student)]["remarks"]
             if [*valueRemarks].__contains__(name_remark):
                 valueRemarks[new_name_remark] = valueRemarks.pop(name_remark)
                 return [*valueRemarks]
