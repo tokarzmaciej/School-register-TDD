252cd10664ab644101fbb6ee51d1f8afd5323294
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 09:21:59 +0100

    test_add_subject_name_subject_bad_type REFACTOR

--- src/subjects.py
+++ src/subjects.py
@@ -5,4 +5,4 @@ class Subjects(Students):
 
     def addSubject(self, id, name_student, surname_student, name_subject):
         if type(name_subject) != str:
-            raise TypeError
+            raise TypeError("Bad_type_subject_name")
