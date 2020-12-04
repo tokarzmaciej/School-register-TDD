fcbda40fbd250262a339ff825c3f8da2dac29893
Maciej <maciej.tokarz1@wp.pl>
2020-12-04 23:31:44 +0100

    test_average_subject_lack_subject REFACTOR

--- src/statistics.py
+++ src/statistics.py
@@ -4,7 +4,10 @@ from src.students import *
 class Statistics(Students):
 
     def averageSubject(self, id, name_student, surname_student, name_subject):
-        if not self.keys.__contains__((str(id), name_student, surname_student)):
+        keys = [*self.students]
+        if keys.__contains__((str(id), name_student, surname_student)):
+            if not [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(
+                    name_subject):
+                raise Exception("Student_not_have_this_subject")
+        else:
             raise Exception("There_is_not_such_student")
-        elif not [*self.students[(str(id), name_student, surname_student)]["subjects"]].__contains__(name_subject):
-            raise Exception("Student_not_have_this_subject")
