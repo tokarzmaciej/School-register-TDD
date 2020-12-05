b223e43c1ff50909daea3916560b18b4fb3cf541
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 10:00:26 +0100

    test_add_subjects_positive HAMCREST

--- tests/test_subjects_add_hamcrest.py
+++ tests/test_subjects_add_hamcrest.py
@@ -7,6 +7,10 @@ class SubjectAddHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Subjects()
 
+    def test_add_subjects_positive(self):
+        result = {'subjects': {"english": {}}, 'remarks': {}}
+        assert_that(self.temp.addSubject(2, "Beata", "Jankowska", "english"), equal_to(result))
+
     def test_add_subject_to_lack_student(self):
         assert_that(calling(self.temp.addSubject)
                     .with_args(12, "Zosia", "Kucharska", "math"),
