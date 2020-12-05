8579fb19441f458ef9b460b10a9b11253d19ce67
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 17:33:13 +0100

    test_edit_remark_exceptions_class parameterized

--- tests/test_edit_remark_parameterized.py
+++ tests/test_edit_remark_parameterized.py
@@ -60,6 +60,31 @@ class RemarksEditParameterizedPackage(unittest.TestCase):
                           self.tmp.editRemarkDescription(self.id, self.name, self.surname, self.name_remark,
                                                          self.new_description_remark))
 
+    @parameterized_class(("id", "name", "surname", "name_remark", "new_name_remark", "expected"), [
+        (1, "Kasia", "Polak", "volunteering", "helping", "Student_not_have_this_remark"),
+        (15, "Wiktor", "Nowak", "competition", "football_competition", "There_is_not_such_student")
+    ])
+    class RemarkEditExceptionsParameterizedPackageClass(unittest.TestCase):
+        def setUp(self):
+            self.tmp = Remarks()
+
+        def test_edit_remark_name_exceptions_class(self):
+            self.assertRaisesRegex(Exception, self.expected, self.tmp.editRemarkName, self.id, self.name, self.surname,
+                                   self.name_remark,
+                                   self.new_name_remark)
+
+    @parameterized_class(("id", "name", "surname", "name_remark", "new_description_remark", "expected"), [
+        (3, "Kacper", "Stoch", "volunteering", "helping_an_elderly_person", "Student_not_have_this_remark"),
+        (14, "Julka", "Ostrowska", "competition", "first_place", "There_is_not_such_student")
+    ])
+    class RemarkEditExceptionsParameterizedPackageClass(unittest.TestCase):
+        def setUp(self):
+            self.tmp = Remarks()
+
+        def test_edit_remark_description_exceptions_class(self):
+            self.assertRaisesRegex(Exception, self.expected, self.tmp.editRemarkDescription, self.id, self.name,
+                                   self.surname, self.name_remark, self.new_description_remark)
+
 
 if __name__ == '__main__':
     unittest.main()
