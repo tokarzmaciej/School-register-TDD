fe62f6ecdfc013ec01ff12fc4d286ba756661be4
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 15:21:04 +0100

    test_add_mark_exceptions_expand parameterized

--- tests/test_add_marks_parameterized.py
+++ tests/test_add_marks_parameterized.py
@@ -11,8 +11,17 @@ class MarksAddParameterizedPackage(unittest.TestCase):
     @parameterized.expand([
         (4, "Alicja", "Zielonka", "math", "test", 2, ('test', 2)),
     ])
-    def test_delete_subjects_positive_expand(self, id, name, surname, name_subject, name_mark, grade, expected):
+    def test_add_mark_positive_expand(self, id, name, surname, name_subject, name_mark, grade, expected):
         self.assertIn(expected, self.tmp.addMark(id, name, surname, name_subject, name_mark, grade))
+
+    @parameterized.expand([
+        (4, "Alicja", "Zielonka", "math", "test", 7, "Bad_range_marks"),
+        (11, "Robert", "Perkoz", "english", "test-present-simple", 3,"There_is_not_such_student"),
+        (3, "Kacper", "Stoch", "geography", "activity", 5,"Student_not_have_this_subject"),
+        (6, "Michal", "Krakowiak", "art", "singing_hymn", 3,"This_mark_already_exists")
+    ])
+    def test_add_mark_exceptions_expand(self, id, name, surname, name_subject, name_mark, grade, expected):
+        self.assertRaisesRegex(Exception, expected, self.tmp.addMark, id, name, surname, name_subject, name_mark, grade)
 
 
 if __name__ == '__main__':
