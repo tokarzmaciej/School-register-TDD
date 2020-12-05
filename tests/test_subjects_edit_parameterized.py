098b6665fbed582dbc58bc80f60a95bc31161a88
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 14:31:04 +0100

    test_edit_subjects_type_errors_expand parameterized

--- tests/test_subjects_edit_parameterized.py
+++ tests/test_subjects_edit_parameterized.py
@@ -18,8 +18,16 @@ class SubjectsEditParameterizedPackage(unittest.TestCase):
         (2, "Grzegorz", "Kowalczyk", "english", "primary_english", "There_is_not_such_student"),
         (4, "Alicja", "Zielonka", "history", "world-history", "Student_not_have_this_subject")
     ])
-    def test_edit_subjects_exceptions_expand(self, id, name, surname, name_subject,new_name_subject, expected):
-        self.assertRaisesRegex(Exception, expected, self.tmp.editSubject, id, name, surname, name_subject,new_name_subject)
+    def test_edit_subjects_exceptions_expand(self, id, name, surname, name_subject, new_name_subject, expected):
+        self.assertRaisesRegex(Exception, expected, self.tmp.editSubject, id, name, surname, name_subject,
+                               new_name_subject)
+
+    @parameterized.expand([
+        (5, "Piotr", "Fantazja", "history", False, "Bad_type_new_subject_name"),
+    ])
+    def test_edit_subjects_type_errors(self, id, name, surname, name_subject, new_name_subject, expected):
+        self.assertRaisesRegex(TypeError, expected, self.tmp.editSubject, id, name, surname, name_subject,
+                               new_name_subject)
 
 
 if __name__ == '__main__':
