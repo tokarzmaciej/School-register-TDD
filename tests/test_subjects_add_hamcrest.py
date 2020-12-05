6a573a1111143b56c863a9b52e61e96c41553146
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 09:53:00 +0100

    test_add_subject_name_subject_bad_type HAMCREST

---
+++ tests/test_subjects_add_hamcrest.py
@@ -0,0 +1,16 @@
+import unittest
+from hamcrest import *
+from src.subjects import Subjects
+
+
+class SubjectAddHamcrestTest(unittest.TestCase):
+    def setUp(self):
+        self.temp = Subjects()
+
+    def test_add_subject_name_subject_bad_type(self):
+        assert_that(calling(self.temp.addSubject)
+                    .with_args(2, "Beata", "Jankowska", True),
+                    raises(TypeError))
+
+    def tearDown(self):
+        self.temp = None
