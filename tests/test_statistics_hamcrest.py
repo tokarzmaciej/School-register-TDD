b9c1fc38cb96e73848e520c4fe5a8fd2fa354685
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 12:54:47 +0100

    test_edit_remark_name_positive HAMCREST

--- tests/test_statistics_hamcrest.py
+++ tests/test_statistics_hamcrest.py
@@ -7,6 +7,11 @@ class AverageHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Statistics()
 
+    def test_edit_remark_name_positive(self):
+        assert_that(
+            self.temp.averageSubject(6, "Michal", "Krakowiak", "math")[1],
+            greater_than_or_equal_to(5))
+
     def test_average_subject_lack_marks(self):
         assert_that(calling(self.temp.averageSubject)
                     .with_args(5, "Piotr", "Fantazja", "art"),
