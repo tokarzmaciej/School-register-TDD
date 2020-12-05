10fe8a18ca7204112cfba582622ef0f9eaa0d45d
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 15:27:30 +0100

    test_add_mark_positive_expand_class parameterized

--- tests/test_add_marks_parameterized.py
+++ tests/test_add_marks_parameterized.py
@@ -16,18 +16,31 @@ class MarksAddParameterizedPackage(unittest.TestCase):
 
     @parameterized.expand([
         (4, "Alicja", "Zielonka", "math", "test", 7, "Bad_range_marks"),
-        (11, "Robert", "Perkoz", "english", "test-present-simple", 3,"There_is_not_such_student"),
-        (3, "Kacper", "Stoch", "geography", "activity", 5,"Student_not_have_this_subject"),
-        (6, "Michal", "Krakowiak", "art", "singing_hymn", 3,"This_mark_already_exists")
+        (11, "Robert", "Perkoz", "english", "test-present-simple", 3, "There_is_not_such_student"),
+        (3, "Kacper", "Stoch", "geography", "activity", 5, "Student_not_have_this_subject"),
+        (6, "Michal", "Krakowiak", "art", "singing_hymn", 3, "This_mark_already_exists")
     ])
     def test_add_mark_exceptions_expand(self, id, name, surname, name_subject, name_mark, grade, expected):
         self.assertRaisesRegex(Exception, expected, self.tmp.addMark, id, name, surname, name_subject, name_mark, grade)
 
     @parameterized.expand([
-        (5, "Piotr", "Fantazja", "math", "test", "1","Bad_type_grade"),
+        (5, "Piotr", "Fantazja", "math", "test", "1", "Bad_type_grade"),
     ])
     def test_add_mark_type_errors_expand(self, id, name, surname, name_subject, name_mark, grade, expected):
         self.assertRaisesRegex(TypeError, expected, self.tmp.addMark, id, name, surname, name_subject, name_mark, grade)
+
+    @parameterized_class(("id", "name", "surname", "name_subject", "new_mark", "grade", "expected"), [
+        (4, "Alicja", "Zielonka", "math", "test", 2, ('test', 2)),
+
+    ])
+    class MarksAddParameterizedPackageClass(unittest.TestCase):
+        def setUp(self):
+            self.tmp = Marks()
+
+        def test_add_mark_positive_expand_class(self):
+            self.assertIn(self.expected, self.tmp.addMark(self.id, self.name, self.surname, self.name_subject,
+                                                          self.new_mark, self.grade))
+
 
 if __name__ == '__main__':
     unittest.main()
