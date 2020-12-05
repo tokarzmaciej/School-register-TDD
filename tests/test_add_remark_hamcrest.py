32e5c6dc3b635e57ea7868013c9a081273870eac
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 11:58:34 +0100

    test_add_remark_already_exists HAMCREST

--- tests/test_add_remark_hamcrest.py
+++ tests/test_add_remark_hamcrest.py
@@ -7,6 +7,11 @@ class RemarksAddHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Remarks()
 
+    def test_add_remark_already_exists(self):
+        assert_that(calling(self.temp.addRemark)
+                    .with_args(8, "Konrad", "Piasek", "competition", "first place"),
+                    raises(Exception))
+
     def test_add_remark_lack_student(self):
         assert_that(calling(self.temp.addRemark)
                     .with_args(10, "Gosia", "Rosa", "help", "preparing_appeal"),
