f4e8abc72d676086b509ff878b6e70e52c6d2541
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 13:50:14 +0100

    test_add_subjects_exceptions_class parameterized

--- tests/test_subjects_add_parameterized.py
+++ tests/test_subjects_add_parameterized.py
@@ -24,9 +24,9 @@ class SubjectsAddParameterizedPackage(unittest.TestCase):
         (2, "Beata", "Jankowska", True, "Bad_type_subject_name"),
     ])
     def test_add_subjects_type_errors(self, id, name, surname, name_subject, expected):
-        self.assertRaisesRegex(Exception, expected, self.tmp.addSubject, id, name, surname, name_subject)
+        self.assertRaisesRegex(TypeError, expected, self.tmp.addSubject, id, name, surname, name_subject)
 
-    @parameterized_class(("id", "name", "surname","name_subject", "expected"), [
+    @parameterized_class(("id", "name", "surname", "name_subject", "expected"), [
         (2, "Beata", "Jankowska", "english", {'subjects': {"english": {}}, 'remarks': {}}),
 
     ])
@@ -36,6 +36,16 @@ class SubjectsAddParameterizedPackage(unittest.TestCase):
 
         def test_add_subjects_positive_class(self):
             self.assertEqual(self.tmp.addSubject(self.id, self.name, self.surname, self.name_subject), self.expected)
+
+    @parameterized_class(("id", "name", "surname", "name_subject", "expected"), [
+        (12, "Zosia", "Kucharska", "math", "There_is_not_such_student"),
+    ])
+    class SubjectsAddExceptionsParameterizedPackageClass(unittest.TestCase):
+        def setUp(self):
+            self.tmp = Subjects()
+
+        def test_add_subjects_exceptions_class(self):
+            self.assertRaisesRegex(Exception, self.expected, self.tmp.addSubject, self.id, self.name, self.surname, self.name_subject)
 
 
 if __name__ == '__main__':
