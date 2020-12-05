ee35cc14633b126267968fea78bac206347385d3
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 15:53:04 +0100

    test_edit_mark_exceptions_expand parameterized

--- tests/test_edit_marks_parameterized.py
+++ tests/test_edit_marks_parameterized.py
@@ -14,6 +14,15 @@ class MarksEditParameterizedPackage(unittest.TestCase):
     def test_edit_mark_positive_expand(self, id, name, surname, name_subject, name_mark, grade, expected):
         self.assertIn(expected, self.tmp.editMark(id, name, surname, name_subject, name_mark, grade))
 
+    @parameterized.expand([
+        (10, "Ewa", "Piech", "english", "quiz", 5, "There_is_not_such_student"),
+        (4, "Alicja", "Zielonka", "history", "test", 3, "Student_not_have_this_subject"),
+        (6, "Michal", "Krakowiak", "math", "exam", 3, "There_is_not_such_mark")
+    ])
+    def test_edit_mark_exceptions_expand(self, id, name, surname, name_subject, name_mark, grade, expected):
+        self.assertRaisesRegex(Exception, expected, self.tmp.editMark, id, name, surname, name_subject, name_mark,
+                               grade)
+
 
 if __name__ == '__main__':
     unittest.main()
