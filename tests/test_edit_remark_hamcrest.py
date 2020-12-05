3e0dbd57b4e4e4809f8e8d030de9f804220bc842
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 12:30:18 +0100

    test_edit_remark_description_lack_remark HAMCREST

--- tests/test_edit_remark_hamcrest.py
+++ tests/test_edit_remark_hamcrest.py
@@ -7,6 +7,11 @@ class RemarksEditHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Remarks()
 
+    def test_edit_remark_description_lack_remark(self):
+        assert_that(calling(self.temp.editRemarkDescription)
+                    .with_args(3, "Kacper", "Stoch", "volunteering", "helping_an_elderly_person"),
+                    raises(Exception))
+
     def test_edit_remark_description_lack_students(self):
         assert_that(calling(self.temp.editRemarkDescription)
                     .with_args(14, "Julka", "Ostrowska", "competition", "first_place"),
