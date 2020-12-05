c27f554288bf17f2e51aee9c70756bcab5a1e751
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 15:32:03 +0100

    test_add_mark_exceptions_expand_class parameterized

--- tests/test_add_marks_parameterized.py
+++ tests/test_add_marks_parameterized.py
@@ -41,6 +41,20 @@ class MarksAddParameterizedPackage(unittest.TestCase):
             self.assertIn(self.expected, self.tmp.addMark(self.id, self.name, self.surname, self.name_subject,
                                                           self.new_mark, self.grade))
 
+    @parameterized_class(("id", "name", "surname", "name_subject", "new_mark", "grade", "expected"), [
+        (4, "Alicja", "Zielonka", "math", "test", 7, "Bad_range_marks"),
+        (11, "Robert", "Perkoz", "english", "test-present-simple", 3, "There_is_not_such_student"),
+        (3, "Kacper", "Stoch", "geography", "activity", 5, "Student_not_have_this_subject"),
+        (6, "Michal", "Krakowiak", "art", "singing_hymn", 3, "This_mark_already_exists")
+    ])
+    class MarksAddExceptionsParameterizedPackageClass(unittest.TestCase):
+        def setUp(self):
+            self.tmp = Marks()
+
+        def test_add_mark_exceptions_expand_class(self):
+            self.assertRaisesRegex(Exception, self.expected, self.tmp.addMark, self.id, self.name, self.surname,
+                                   self.name_subject, self.new_mark, self.grade)
+
 
 if __name__ == '__main__':
     unittest.main()
