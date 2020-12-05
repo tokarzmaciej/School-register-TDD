e090d7771d277c98619fc25e8ba0ab59e2b9759d
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 10:55:20 +0100

    test_add_mark_range_mark HAMCREST

--- tests/test_marks_add_hamcrest.py
+++ tests/test_marks_add_hamcrest.py
@@ -7,6 +7,11 @@ class MarksAddHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Marks()
 
+    def test_add_mark_range_mark(self):
+        assert_that(calling(self.temp.addMark)
+                    .with_args(4, "Alicja", "Zielonka", "math", "test", 7),
+                    raises(Exception))
+
     def test_add_mark_name_bad_type(self):
         assert_that(calling(self.temp.addMark)
                     .with_args(5, "Piotr", "Fantazja", "math", "test", "1"),
