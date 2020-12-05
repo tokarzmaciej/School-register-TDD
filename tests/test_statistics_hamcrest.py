f0661a40141bb072748d50b460ae5acd2fc9053e
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 12:52:04 +0100

    test_average_subject_lack_marks HAMCREST

--- tests/test_statistics_hamcrest.py
+++ tests/test_statistics_hamcrest.py
@@ -7,6 +7,11 @@ class AverageHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Statistics()
 
+    def test_average_subject_lack_marks(self):
+        assert_that(calling(self.temp.averageSubject)
+                    .with_args(5, "Piotr", "Fantazja", "art"),
+                    raises(Exception))
+
     def test_average_subject_lack_subject(self):
         assert_that(calling(self.temp.averageSubject)
                     .with_args(4, "Alicja", "Zielonka", "history"),
