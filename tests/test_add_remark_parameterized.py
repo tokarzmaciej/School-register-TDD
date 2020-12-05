dca86670d3be57fa17d114cedf761f49bf0a1104
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 16:15:01 +0100

    test_add_remark_exceptions_positive_expand parameterized

--- tests/test_add_remark_parameterized.py
+++ tests/test_add_remark_parameterized.py
@@ -15,6 +15,13 @@ class RemarksAddParameterizedPackage(unittest.TestCase):
     def test_add_remark_positive_expand(self, id, name, surname, name_remark, description_remark, expected):
         self.assertEqual(expected, self.tmp.addRemark(id, name, surname, name_remark, description_remark))
 
+    @parameterized.expand([
+        (8, "Konrad", "Piasek", "competition", "first place", "This_remark_already_exists"),
+        (10, "Gosia", "Rosa", "help", "preparing_appeal", "There_is_not_such_student")
+    ])
+    def test_add_remark_exceptions_positive_expand(self, id, name, surname, name_remark, description_remark, expected):
+        self.assertRaisesRegex(Exception, expected, self.tmp.addRemark, id, name, surname, name_remark, description_remark)
+
 
 if __name__ == '__main__':
     unittest.main()
