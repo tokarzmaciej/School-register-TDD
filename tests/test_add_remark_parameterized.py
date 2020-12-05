46c107b17a8d241f3d883da45e42c4828658d7ba
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 16:20:08 +0100

    test_add_remark_positive_class parameterized

--- tests/test_add_remark_parameterized.py
+++ tests/test_add_remark_parameterized.py
@@ -20,7 +20,20 @@ class RemarksAddParameterizedPackage(unittest.TestCase):
         (10, "Gosia", "Rosa", "help", "preparing_appeal", "There_is_not_such_student")
     ])
     def test_add_remark_exceptions_positive_expand(self, id, name, surname, name_remark, description_remark, expected):
-        self.assertRaisesRegex(Exception, expected, self.tmp.addRemark, id, name, surname, name_remark, description_remark)
+        self.assertRaisesRegex(Exception, expected, self.tmp.addRemark, id, name, surname, name_remark,
+                               description_remark)
+
+    @parameterized_class(("id", "name", "surname", "name_remark", "description_remark", "expected"), [
+        (2, "Beata", "Jankowska", "volunteering", "cleaning_the_environment",
+         {'volunteering': 'cleaning_the_environment'})
+    ])
+    class RemarkAddParameterizedPackageClass(unittest.TestCase):
+        def setUp(self):
+            self.tmp = Remarks()
+
+        def test_add_remark_positive_class(self):
+            self.assertEqual(self.tmp.addRemark(self.id, self.name, self.surname, self.name_remark,
+                                                self.description_remark), self.expected)
 
 
 if __name__ == '__main__':
