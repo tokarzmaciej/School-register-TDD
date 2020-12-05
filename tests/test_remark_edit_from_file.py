f23932b7358c6372719997bdcbe7d127a02f081c
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 20:50:31 +0100

    test_edit_remark_description_and_name_exceptions_from_file

--- tests/test_remark_edit_from_file.py
+++ tests/test_remark_edit_from_file.py
@@ -3,9 +3,11 @@ from src.remark import *
 
 
 class RemarkEditParameterizedFile(unittest.TestCase):
+    def setUp(self):
+        self.tmp = Remarks()
+
     def test_edit_remark_name_from_file(self):
         fileTest = open("data/test_remark_edit_name.txt")
-        tmp = Remarks()
         for line in fileTest:
             if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
                 continue
@@ -13,13 +15,12 @@ class RemarkEditParameterizedFile(unittest.TestCase):
                 data = line.split(" ")
                 inp1, inp2, inp3, inp4, inp5, inp6 = (
                     data[0], data[1], data[2], data[3], data[4], data[5].strip("\n"))
-                self.assertIn(inp6, tmp.editRemarkName(inp1, inp2, inp3, inp4, inp5)[1])
+                self.assertIn(inp6, self.tmp.editRemarkName(inp1, inp2, inp3, inp4, inp5)[1])
 
         fileTest.close()
 
     def test_edit_remark_description_from_file(self):
         fileTest = open("data/test_remark_edit_description.txt")
-        tmp = Remarks()
         for line in fileTest:
             if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
                 continue
@@ -27,9 +28,33 @@ class RemarkEditParameterizedFile(unittest.TestCase):
                 data = line.split(" ")
                 inp1, inp2, inp3, inp4, inp5, inp6 = (
                     data[0], data[1], data[2], data[3], data[4], data[5].strip("\n"))
-                self.assertIn(inp6, tmp.editRemarkDescription(inp1, inp2, inp3, inp4, inp5))
+                self.assertIn(inp6, self.tmp.editRemarkDescription(inp1, inp2, inp3, inp4, inp5))
 
         fileTest.close()
+
+    def test_edit_remark_name_exceptions_from_file(self):
+        fileTest = open("data/test_remark_edit_name_exceptions.txt")
+        for line in fileTest:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                data = line.split(" ")
+                inp1, inp2, inp3, inp4, inp5, inp6 = (
+                    data[0], data[1], data[2], data[3], data[4], data[5].strip("\n"))
+                self.assertRaisesRegex(Exception, inp6, self.tmp.editRemarkName, inp1, inp2, inp3, inp4, inp5)
+
+        fileTest.close()
+
+    def test_edit_remark_description_exceptions_from_file(self):
+        fileTest = open("data/test_remark_edit_description_exceptions.txt")
+        for line in fileTest:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                data = line.split(" ")
+                inp1, inp2, inp3, inp4, inp5, inp6 = (
+                    data[0], data[1], data[2], data[3], data[4], data[5].strip("\n"))
+                self.assertRaisesRegex(Exception, inp6, self.tmp.editRemarkDescription, inp1, inp2, inp3, inp4, inp5)
 
 
 if __name__ == '__main__':
