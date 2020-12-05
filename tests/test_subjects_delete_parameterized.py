b665ce4260766cb8b1a5b9f8df4aeb21610dda5c
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 14:00:19 +0100

    test_delete_subjects_exceptions_expand parameterized

--- tests/test_subjects_delete_parameterized.py
+++ tests/test_subjects_delete_parameterized.py
@@ -14,6 +14,13 @@ class SubjectsDeleteParameterizedPackage(unittest.TestCase):
     def test_delete_subjects_positive_expand(self, id, name, surname, name_subject, expected):
         self.assertEqual(self.tmp.deleteSubject(id, name, surname, name_subject), expected)
 
+    @parameterized.expand([
+        (10, "Ania", "Zegan", "english", "There_is_not_such_student"),
+        (5, "Piotr", "Fantazja", "geography", "Student_not_have_this_subject")
+    ])
+    def test_delete_subjects_exceptions_expand(self, id, name, surname, name_subject, expected):
+        self.assertRaisesRegex(Exception, expected, self.tmp.deleteSubject, id, name, surname, name_subject)
+
 
 if __name__ == '__main__':
     unittest.main()
