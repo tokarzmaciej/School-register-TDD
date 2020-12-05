8c933c96ab878b423e2fc9193e11efbdf507644d
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 10:07:02 +0100

    test_delete_subject_name_bad_typ HAMCREST

---
+++ tests/test_subjects_delete_hamcrest.py
@@ -0,0 +1,16 @@
+import unittest
+from hamcrest import *
+from src.subjects import Subjects
+
+
+class StudentsHamcrestTest(unittest.TestCase):
+    def setUp(self):
+        self.temp = Subjects()
+
+    def test_delete_subject_name_bad_typ(self):
+        assert_that(calling(self.temp.deleteSubject)
+                    .with_args(5, "Piotr", "Fantazja", 1234),
+                    raises(TypeError))
+
+    def tearDown(self):
+        self.temp = None
