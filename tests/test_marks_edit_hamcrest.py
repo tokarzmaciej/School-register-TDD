4d91dfc3d7f8809939eed94fd2d22ba73cb29512
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 11:37:40 +0100

    test_edit_mark_lack_student HAMCREST

--- tests/test_marks_edit_hamcrest.py
+++ tests/test_marks_edit_hamcrest.py
@@ -7,6 +7,11 @@ class MarksEditHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Marks()
 
+    def test_edit_mark_lack_student(self):
+        assert_that(calling(self.temp.editMark)
+                    .with_args(10, "Ewa", "Piech", "english", "quiz", 5),
+                    raises(Exception))
+
     def test_edit_mark_grade_bad_type(self):
         assert_that(calling(self.temp.editMark)
                     .with_args(7, "Ewelina", "Swoboda", "physics", "quiz", False),
