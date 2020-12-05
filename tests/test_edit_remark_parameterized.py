42100983a706eedab56bf8faae8fdfba53a456f8
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 17:26:07 +0100

    test_edit_remark_class parameterized

--- tests/test_edit_remark_parameterized.py
+++ tests/test_edit_remark_parameterized.py
@@ -21,8 +21,8 @@ class RemarksEditParameterizedPackage(unittest.TestCase):
         self.assertIn(expected, self.tmp.editRemarkDescription(id, name, surname, name_remark, new_description_remark))
 
     @parameterized.expand([
-        (1, "Kasia", "Polak", "volunteering", "helping","Student_not_have_this_remark"),
-        (15, "Wiktor", "Nowak", "competition", "football_competition","There_is_not_such_student")
+        (1, "Kasia", "Polak", "volunteering", "helping", "Student_not_have_this_remark"),
+        (15, "Wiktor", "Nowak", "competition", "football_competition", "There_is_not_such_student")
     ])
     def test_edit_remark_name_exceptions_expand(self, id, name, surname, name_remark, new_name_remark, expected):
         self.assertRaisesRegex(Exception, expected, self.tmp.editRemarkName, id, name, surname, name_remark,
@@ -36,6 +36,29 @@ class RemarksEditParameterizedPackage(unittest.TestCase):
                                                        expected):
         self.assertRaisesRegex(Exception, expected, self.tmp.editRemarkDescription, id, name, surname, name_remark,
                                new_description_remark)
+
+    @parameterized_class(("id", "name", "surname", "name_remark", "new_name_remark", "expected"), [
+        (8, "Konrad", "Piasek", "disturbing_lesson", "disturbing_math_lesson", "math_lesson")
+    ])
+    class RemarkEditParameterizedPackageClass(unittest.TestCase):
+        def setUp(self):
+            self.tmp = Remarks()
+
+        def test_edit_remark_name_class(self):
+            self.assertIn(self.expected, self.tmp.editRemarkName(self.id, self.name, self.surname, self.name_remark,
+                                                                 self.new_name_remark)[1])
+
+    @parameterized_class(("id", "name", "surname", "name_remark", "new_description_remark", "expected"), [
+        (7, "Ewelina", "Swoboda", "competition", "second_place", "second")
+    ])
+    class RemarkEditParameterizedPackageClass(unittest.TestCase):
+        def setUp(self):
+            self.tmp = Remarks()
+
+        def test_edit_remark_description_class(self):
+            self.assertIn(self.expected,
+                          self.tmp.editRemarkDescription(self.id, self.name, self.surname, self.name_remark,
+                                                         self.new_description_remark))
 
 
 if __name__ == '__main__':
