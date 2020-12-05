50c1f1a8b78b93767aa47771e517c0e9490637b5
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 10:25:28 +0100

    test_edit_subject_to_lack_student HAMCREST

--- tests/test_subjects_edit_hamcrest.py
+++ tests/test_subjects_edit_hamcrest.py
@@ -7,8 +7,13 @@ class SubjectsEditHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Subjects()
 
+    def test_edit_subject_to_lack_student(self):
+        assert_that(calling(self.temp.editSubject)
+                    .with_args(2, "Grzegorz", "Kowalczyk", "english", "primary_english"),
+                    raises(Exception))
+
     def test_edit_subject_name_bad_type(self):
-        assert_that(calling(self.temp.deleteSubject)
+        assert_that(calling(self.temp.editSubject)
                     .with_args(5, "Piotr", "Fantazja", "history", False),
                     raises(TypeError))
 
