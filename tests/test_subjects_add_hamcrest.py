c31615e831d2faa43cd78cf95407e59cc0509538
Maciej <maciej.tokarz1@wp.pl>
2020-12-05 09:54:28 +0100

    test_add_subject_to_lack_student HAMCREST

--- tests/test_subjects_add_hamcrest.py
+++ tests/test_subjects_add_hamcrest.py
@@ -7,6 +7,11 @@ class SubjectAddHamcrestTest(unittest.TestCase):
     def setUp(self):
         self.temp = Subjects()
 
+    def test_add_subject_to_lack_student(self):
+        assert_that(calling(self.temp.addSubject)
+                    .with_args(12, "Zosia", "Kucharska", "math"),
+                    raises(Exception))
+
     def test_add_subject_name_subject_bad_type(self):
         assert_that(calling(self.temp.addSubject)
                     .with_args(2, "Beata", "Jankowska", True),
